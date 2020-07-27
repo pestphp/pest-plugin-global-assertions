271c96dd4dace5279b8c67a7b2a4ffb6172060a4
Owen Voke <development@voke.dev>
2020-07-27 13:20:58 +0100

    build: add script for compiling assertions

---
+++ scripts/compile.php
@@ -0,0 +1,28 @@
+<?php 
+
+declare(strict_types=1);
+
+$globalsFilePath = __DIR__ . '/vendor/phpunit/phpunit/src/Framework/Assert/Functions.php';
+
+$compiledFilePath = __DIR__ . '/src/compiled.php';
+
+@unlink($compiledFilePath);
+
+$replace = function ($contents, $string, $by) { 
+    return str_replace($string, $by, $contents);
+} 
+
+
+$remove = function ($contents, $string) {
+    return str_replace($string, '', $contents);
+};
+
+$contents = file_get_contents($globalsFilePath);
+$contents = $replace($contents, 'namespace PHPUnit\Framework;', 'use PHPUnit\Framework\Assert;');
+$contents = $remove($contents, 'use ArrayAccess;');
+$contents = $remove($contents, 'use Countable;');
+$contents = $remove($contents, 'use DOMDocument;');
+$contents = $remove($contents, 'use DOMElement;');
+$contents = $remove($contents, 'use Throwable;');
+
+file_put_contents($compiledFilePath, $contents);
