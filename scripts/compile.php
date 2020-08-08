254974d412e2f0762f588eb8ca1a2128230b5b43
Owen Voke <development@voke.dev>
2020-08-08 15:12:18 +0100

    build: update script for compiling functions

--- scripts/compile.php
+++ scripts/compile.php
@@ -2,16 +2,15 @@
 
 declare(strict_types=1);
 
-$globalsFilePath = __DIR__ . '/vendor/phpunit/phpunit/src/Framework/Assert/Functions.php';
+$globalsFilePath = __DIR__ . '/../vendor/phpunit/phpunit/src/Framework/Assert/Functions.php';
 
-$compiledFilePath = __DIR__ . '/src/compiled.php';
+$compiledFilePath = __DIR__ . '/../src/compiled.php';
 
 @unlink($compiledFilePath);
 
-$replace = function ($contents, $string, $by) { 
+$replace = function ($contents, $string, $by) {
     return str_replace($string, $by, $contents);
-} 
-
+};
 
 $remove = function ($contents, $string) {
     return str_replace($string, '', $contents);
@@ -19,6 +18,9 @@ $remove = function ($contents, $string) {
 
 $contents = file_get_contents($globalsFilePath);
 $contents = $replace($contents, 'namespace PHPUnit\Framework;', 'use PHPUnit\Framework\Assert;');
+$contents = $remove($contents, 'use function define;');
+$contents = $remove($contents, 'use function defined;');
+$contents = $remove($contents, 'use function func_get_args;');
 $contents = $remove($contents, 'use ArrayAccess;');
 $contents = $remove($contents, 'use Countable;');
 $contents = $remove($contents, 'use DOMDocument;');
