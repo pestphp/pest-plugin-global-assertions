e3b082652bdf8e447170968595f6b9a33e18d588
Owen Voke <development@voke.dev>
2020-11-25 12:30:06 +0000

    feat: update script for latest PHPUnit

--- scripts/compile.php
+++ scripts/compile.php
@@ -17,9 +17,14 @@ $remove = function ($contents, $string) {
 };
 
 $contents = file_get_contents($globalsFilePath);
-$contents = $replace($contents, 'namespace PHPUnit\Framework;', 'use PHPUnit\Framework\Assert;');
-$contents = $remove($contents, 'use function define;');
-$contents = $remove($contents, 'use function defined;');
+$contents = $replace($contents, 'namespace PHPUnit\Framework;', <<<'PHP'
+if (defined('__PEST_GLOBAL_ASSERT_WRAPPERS__')) {
+    return;
+}
+define('__PEST_GLOBAL_ASSERT_WRAPPERS__', true);
+
+use PHPUnit\Framework\Assert;
+PHP);
 $contents = $remove($contents, 'use function func_get_args;');
 $contents = $remove($contents, 'use ArrayAccess;');
 $contents = $remove($contents, 'use Countable;');
@@ -28,7 +33,6 @@ $contents = $remove($contents, 'use DOMElement;');
 $contents = $remove($contents, 'use Throwable;');
 
 // Fix for order of autoloading files
-$contents = $replace($contents, "if (!\\defined('__PHPUNIT_GLOBAL_ASSERT_WRAPPERS__')) {", "if (!\\defined('__PEST_GLOBAL_ASSERT_WRAPPERS__')) {");
-$contents = $replace($contents, "\\define('__PHPUNIT_GLOBAL_ASSERT_WRAPPERS__', true);\n}", "\\define('__PEST_GLOBAL_ASSERT_WRAPPERS__', true);\n}");
+$contents = $replace($contents, "if (!function_exists('PHPUnit\\Framework\\", "if (!function_exists('");
 
 file_put_contents($compiledFilePath, $contents);
